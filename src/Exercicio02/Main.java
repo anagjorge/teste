a18cf7a5266261c2b84a30ce6a5aca4c9d475e84
Ana <anajessica@planeta.inf.br>
2022-04-07 10:55:40 -0300

    modified

--- src/Exercicio02/Main.java
+++ src/Exercicio02/Main.java
@@ -3,33 +3,40 @@ package Exercicio02;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		Scanner leitor = new Scanner(System.in); 
-		
-		System.out.println("Digite quantos números deseja inserir");
+
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite quantos valores deseja adicionar");
 		int n = leitor.nextInt();
-		int qtd[] = new int [n];  
-		
-		int i = 0;
-		int pares = 0;
-		if(qtd[i] %2 ==0 ) {
-			pares ++;
-		}
-		int impares = 0;
-		if(qtd[i] % 2 == 1) {
-			impares ++;
-		}
-		int qtdRepetido = 0;
-		if(qtd[i] == qtd[i]) {
-			System.out.println("Os repetidos são:" + qtd[i]);
-			qtdRepetido++;
-		}	
-			
-		
-		System.out.println("Pares:" + pares);
-		System.out.println("Impares:" + impares);
-		System.out.println("Quantidade repetidos:" +qtdRepetido);
+		int qtd[] = new int[n];
+		int i = 0, pares = 0, repetidos = 0, impares = 0;
+
+		for (i = 0; i < n; i++) {
+			System.out.println("Digite um número:");
+			qtd[i] = leitor.nextInt();
+
 		}
 
+		for (i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (qtd[i] == qtd[j]) {
+					System.out.println("Valor repetido :" + qtd[i]);
+					repetidos++;
+				}
+			}
+			if (qtd[i] % 2 == 0) {
+				pares++;
+			}
+
+			if (qtd[i] % 2 == 1) {
+				impares++;
+			}
+		}
+		System.out.println("");
+		System.out.println("Quantidade de pares:" + pares);
+		System.out.println("Quantidade de ímpares:" + impares);
+		System.out.println("Quantidade de repetidos:" + repetidos);
+
+	}
 }
